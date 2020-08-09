bd4eb4471c211eec4b8b8e6fe77ac9386826c508
DDAkebono <gandvando@hotmail.com>
2020-08-08 20:56:26 -0500

    Initial version 1.0.0 commit

---
+++ BTKSAGestureMod.cs
@@ -0,0 +1,70 @@
+﻿using Harmony;
+using MelonLoader;
+using System.Reflection;
+
+namespace BTKSAGestureMod
+{
+    public static class BuildInfo
+    {
+        public const string Name = "BTKSAGestureMod"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "DDAkebono#0001"; // Author of the Mod.  (Set as null if none)
+        public const string Company = "BTK-Development"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/ddakebono/BTKSAGestureMod/releases"; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class BTKSAGestureMod : MelonMod
+    {
+        public static BTKSAGestureMod instance;
+
+        public HarmonyInstance harmony;
+
+        public static string settingsCategory = "BTKSAGestureMod";
+        public static string rightHandSetting = "righthand";
+        public static string leftHandSetting = "lefthand";
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            MelonLogger.Log("BTK Standalone: Gesture Mod - Starting up");
+
+            instance = this;
+
+            MelonPrefs.RegisterCategory(settingsCategory, "Gesture Mod");
+            MelonPrefs.RegisterBool(settingsCategory, rightHandSetting, false, "Replace Right Hand Action Menu");
+            MelonPrefs.RegisterBool(settingsCategory, leftHandSetting, false, "Replace Left Hand Action Menu");
+
+
+            //Initalize Harmony
+            harmony = HarmonyInstance.Create("BTKStandalone");
+            //OpenActionMenu - Takes bool for open or close?
+            harmony.Patch(typeof(ActionMenuOpener).GetMethod("Method_Public_Void_Boolean_2", BindingFlags.Public | BindingFlags.Instance), new HarmonyMethod(typeof(BTKSAGestureMod).GetMethod("OnActionMenuOpen", BindingFlags.Static | BindingFlags.Public)));
+
+
+        }
+        /// <summary>
+        /// This function will handle the main override of the ActionMenuOpener object according
+        /// to the configuration supplied by MelonPrefs
+        /// </summary>
+        /// <param name="__0">Target state for the ActionMenu, either open or close</param>
+        /// <param name="__instance">Instance of ActionMenuOpener __instance.name gives the MenuL or MenuR needed to determine hand</param>
+        public static bool OnActionMenuOpen(bool __0, ref ActionMenuOpener __instance)
+        {
+            //MelonLogger.Log($"ActionMenuOpener OpenActionMenu Called OpenerName: {__instance.name}, BoolState: {__0}");
+            if ((MelonPrefs.GetBool(settingsCategory, rightHandSetting) && __instance.name.Equals("MenuR")) || (MelonPrefs.GetBool(settingsCategory, leftHandSetting) && __instance.name.Equals("MenuL")))
+            {
+                if (__0)
+                {
+                    HandGestureController.Method_Public_Static_Void_Boolean_0(!HandGestureController.field_Private_Static_Boolean_0);
+                }
+                else
+                {
+                    HandGestureController.Method_Public_Static_Void_Boolean_0(__0);
+                }
+                return false; //Skip original function
+
+            }
+
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BTKSAGestureMod
-VRChat modification to override the ActionMenu buttons with gesture lock
+BTK Standalone: Gesture Mod is a small mod to allow you to switch the ActionMenu button binds back into the Gesture Lock, like how it worked originally on the Index controllers.
+
+The mod has 2 settings that are controlled via [UIExpanionKit](https://github.com/knah/VRCMods), by default neither right or left are overriden, you can choose either, one, both, or none by setting them in UIExpanionKit.
+
+If you encounter issues with this mod, feel free to contact me on Discord (DDAkebono#0001)!
+
+## Install/Usage
+Install [MelonLoader](https://github.com/HerpDerpinstine/MelonLoader) version 0.2.6 or higher, it does require 0.2.6 as it has migrated to the new MelonPrefs and MelonLogger classes.
+
+Download the latest release from [Releases](https://github.com/ddakebono/BTKSAGestureMod/releases) and place in your VRChat\Mods folder.
+
+The mod will initially only be fixing the nameplate position, but you can enable the hidden custom nameplate feature in the UIExpanionKit settings menu.
+
+## Disclamer
+**Warning:** The VRChat team is not very keen on modding or reverse engineering the game, while the mod does not include anything that would ruin the fun for others, using it may still be a bannable offence.
+
+**USE IT AT YOUR OWN RISK**, I am not responsible for any bans or any punishments you may get by using this mod!
+
+## Credits
+* [HerpDerpinstine/MelonLoader](https://github.com/HerpDerpinstine/MelonLoader)
