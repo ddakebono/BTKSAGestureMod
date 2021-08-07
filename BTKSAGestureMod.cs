c4eef053b3c8c7dfe03ebaf69dac609edae664a8
DDAkebono <gandvando@hotmail.com>
2021-08-07 05:27:56 -0500

    Readded XRDevice.IsPresent check, oops

--- BTKSAGestureMod.cs
+++ BTKSAGestureMod.cs
@@ -13,7 +13,7 @@ namespace BTKSAGestureMod
         public const string Name = "BTKSAGestureMod";
         public const string Author = "DDAkebono#0001";
         public const string Company = "BTK-Development";
-        public const string Version = "1.1.8";
+        public const string Version = "1.1.9";
         public const string DownloadLink = "https://github.com/ddakebono/BTKSAGestureMod/releases";
     }
 
@@ -60,7 +60,7 @@ namespace BTKSAGestureMod
             MelonPreferences.CreateEntry<bool>(settingsCategory, leftHandActionDisable, false, "Disable Left Action Menu");
 
             //Only initialize for VR users
-            if (true)//XRDevice.isPresent)
+            if (XRDevice.isPresent)
             {
                 HandGestureCtrlMethod = typeof(HandGestureController).GetMethods(BindingFlags.Static | BindingFlags.Public).FirstOrDefault(x => x.Name.Contains("Method_Public_Static_Void_Boolean_"));
                 if (HandGestureCtrlMethod == null)
