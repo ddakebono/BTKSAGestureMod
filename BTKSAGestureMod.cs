1a99911603aa6b2b2eb54a95ddd99363f8a88214
DDAkebono <gandvando@hotmail.com>
2021-08-07 19:05:11 -0500

    Made the patch not be applied at all when not in vr

--- BTKSAGestureMod.cs
+++ BTKSAGestureMod.cs
@@ -13,7 +13,7 @@ namespace BTKSAGestureMod
         public const string Name = "BTKSAGestureMod";
         public const string Author = "DDAkebono#0001";
         public const string Company = "BTK-Development";
-        public const string Version = "1.1.9";
+        public const string Version = "1.1.10";
         public const string DownloadLink = "https://github.com/ddakebono/BTKSAGestureMod/releases";
     }
 
@@ -22,21 +22,20 @@ namespace BTKSAGestureMod
         public static BTKSAGestureMod Instance;
 
         public MethodInfo HandGestureCtrlMethod;
-        public bool IsGestureModReady;
 
-        public static string settingsCategory = "BTKSAGestureMod";
-        public static string rightHandSetting = "righthand";
-        public static string leftHandSetting = "lefthand";
-        public static string leftHandActionDisable = "lefthandactiondisable";
-        public static string rightHandActionDisable = "righthandactiondisable";
+        public static readonly string SettingsCategory = "BTKSAGestureMod";
+        public static readonly string RightHandSetting = "righthand";
+        public static readonly string leftHandSetting = "lefthand";
+        public static readonly string LeftHandActionDisable = "lefthandactiondisable";
+        public static readonly string RightHandActionDisable = "righthandactiondisable";
 
-        int scenesLoaded = 0;
+        int _scenesLoaded = 0;
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            if (scenesLoaded > 2) return;
-            scenesLoaded++;
-            if (scenesLoaded == 2)
+            if (_scenesLoaded > 2) return;
+            _scenesLoaded++;
+            if (_scenesLoaded == 2)
                 UiManagerInit();
         }
 
@@ -53,24 +52,40 @@ namespace BTKSAGestureMod
                 return;
             }
 
-            MelonPreferences.CreateCategory(settingsCategory, "Gesture Mod");
-            MelonPreferences.CreateEntry<bool>(settingsCategory, rightHandSetting, false, "Replace Right Hand Action Menu");
-            MelonPreferences.CreateEntry<bool>(settingsCategory, leftHandSetting, false, "Replace Left Hand Action Menu");
-            MelonPreferences.CreateEntry<bool>(settingsCategory, rightHandActionDisable, false, "Disable Right Action Menu");
-            MelonPreferences.CreateEntry<bool>(settingsCategory, leftHandActionDisable, false, "Disable Left Action Menu");
+            MelonPreferences.CreateCategory(SettingsCategory, "Gesture Mod");
+            MelonPreferences.CreateEntry<bool>(SettingsCategory, RightHandSetting, false, "Replace Right Hand Action Menu");
+            MelonPreferences.CreateEntry<bool>(SettingsCategory, leftHandSetting, false, "Replace Left Hand Action Menu");
+            MelonPreferences.CreateEntry<bool>(SettingsCategory, RightHandActionDisable, false, "Disable Right Action Menu");
+            MelonPreferences.CreateEntry<bool>(SettingsCategory, LeftHandActionDisable, false, "Disable Left Action Menu");
 
             //Only initialize for VR users
             if (XRDevice.isPresent)
             {
                 HandGestureCtrlMethod = typeof(HandGestureController).GetMethods(BindingFlags.Static | BindingFlags.Public).FirstOrDefault(x => x.Name.Contains("Method_Public_Static_Void_Boolean_"));
                 if (HandGestureCtrlMethod == null)
+                {
                     MelonLogger.Error("Could not find required method in HandGestureController! GestureMod will not function.");
-
-                IsGestureModReady = true;
+                    return;
+                }
+                
+                applyPatches(typeof(ActionMenuPatches));
             }
             else
             {
                 MelonLogger.Msg("Desktop Mode Detected, Gesture Mod has not started up!");
+            }
+        }
+        
+        private void applyPatches(Type type)
+        {
+            try
+            {
+                HarmonyLib.Harmony.CreateAndPatchAll(type, "BTKHarmonyInstance");
+            }
+            catch(Exception e)
+            {
+                MelonLogger.Error($"Failed while patching {type.Name}!");
+                MelonLogger.Error(e);
             }
         }
     }
@@ -91,11 +106,9 @@ namespace BTKSAGestureMod
         /// <param name="__instance">Instance of ActionMenuOpener __instance.name gives the MenuL or MenuR needed to determine hand</param>
         static bool Prefix(bool __0, ref ActionMenuOpener __instance)
         {
-            if (!BTKSAGestureMod.Instance.IsGestureModReady) return true;
-            
-            if ((MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.settingsCategory, BTKSAGestureMod.rightHandSetting) &&
+            if ((MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.SettingsCategory, BTKSAGestureMod.RightHandSetting) &&
                  __instance.name.Equals("MenuR")) ||
-                (MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.settingsCategory, BTKSAGestureMod.leftHandSetting) &&
+                (MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.SettingsCategory, BTKSAGestureMod.leftHandSetting) &&
                  __instance.name.Equals("MenuL")) && BTKSAGestureMod.Instance.HandGestureCtrlMethod != null)
             {
 
@@ -115,9 +128,9 @@ namespace BTKSAGestureMod
             }
 
             //Check if action menu is disabled for hand
-            if ((MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.settingsCategory, BTKSAGestureMod.rightHandActionDisable) &&
+            if ((MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.SettingsCategory, BTKSAGestureMod.RightHandActionDisable) &&
                  __instance.name.Equals("MenuR")) ||
-                (MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.settingsCategory, BTKSAGestureMod.leftHandActionDisable) &&
+                (MelonPreferences.GetEntryValue<bool>(BTKSAGestureMod.SettingsCategory, BTKSAGestureMod.LeftHandActionDisable) &&
                  __instance.name.Equals("MenuL")))
                 return false;
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,8 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.9")]
-[assembly: AssemblyFileVersion("1.1.9")]
+[assembly: AssemblyVersion("1.1.10")]
+[assembly: AssemblyFileVersion("1.1.10")]
+[assembly: HarmonyDontPatchAll]
 [assembly: MelonInfo(typeof(BTKSAGestureMod.BTKSAGestureMod), BTKSAGestureMod.BuildInfo.Name, BTKSAGestureMod.BuildInfo.Version, BTKSAGestureMod.BuildInfo.Author, BTKSAGestureMod.BuildInfo.DownloadLink)]
 [assembly: MelonGame("VRChat", "VRChat")]
